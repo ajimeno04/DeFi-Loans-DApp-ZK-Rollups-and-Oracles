d7e3c601e99ee7a61bd2727f15f31bbd2bcef175
ajimeno04 <jimenobeltran11@gmail.com>
2024-11-24 20:21:31 +0100

    feat: update interfaces

--- src/core/interfaces/ISupplyVault.sol
+++ src/core/interfaces/ISupplyVault.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.0;
 
 interface ISupplyVault {
+    function deposit(uint256 amount) external;
+    function withdraw(uint256 amount) external;
     function getCollateral(address user) external view returns (uint256);
     function reduceCollateral(address user, uint256 amount) external;
 }
